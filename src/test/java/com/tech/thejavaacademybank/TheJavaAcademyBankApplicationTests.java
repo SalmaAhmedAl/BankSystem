79b2e80789c417e0ebd2f202bb2b7bf0c1cbc5f8
Salma Ahmed <salmahamzaahmed77@gmail.com>
2024-02-04 01:36:08 +0200

    init commit

---
+++ src/test/java/com/tech/thejavaacademybank/TheJavaAcademyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.thejavaacademybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaAcademyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
